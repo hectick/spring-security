80888bfe421355c89479cd3373c15e62e28d7929
hectick <ch.yeondev@gmail.com>
2024-06-24 01:13:49 +0900

    feat: 로그인한 사용자의 세션 정보를 확인해보기

--- src/main/java/com/example/testsecurity/controller/MainController.java
+++ src/main/java/com/example/testsecurity/controller/MainController.java
@@ -1,13 +1,30 @@
 package com.example.testsecurity.controller;
 
+import java.util.Collection;
+import java.util.Iterator;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String mainPage() {
+    public String mainPage(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String id = authentication.getName(); //id값
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority(); //role값
+
+        model.addAttribute("id", id);
+        model.addAttribute("role", role);
         return "main";
     }
 }
